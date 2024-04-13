8de20928862a84f28feff7f167c40b3c2d4e14c2
shramana-r <shramanar1387@gmail.com>
2024-04-13 22:36:40 +0530

    6. odd.java

---
+++ Practice_Java/6. odd.java
@@ -0,0 +1,11 @@
+package Practice_Java;
+
+class odd {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 50; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
